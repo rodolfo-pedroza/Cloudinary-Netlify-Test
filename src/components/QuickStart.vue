9bc2afbeff3a9b88227f17e83a832dc3aa84fb7b
rodolfo-pedroza <rudy_pedroza@hotmail.com>
2023-01-03 12:08:34 -0600

    test

---
+++ src/components/QuickStart.vue
@@ -0,0 +1,36 @@
+<script>
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from "@cloudinary/vue";
+import { fill } from "@cloudinary/url-gen/actions/resize";
+
+// Create a Cloudinary instance and set your cloud name.
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: "dtrkstqmm",
+  },
+});
+
+// Instantiate a CloudinaryImage object for the image with the public ID, 'docs/models'.
+const myImg = cld.image("cheesecake");
+
+// Resize to 250 x 250 pixels using the 'fill' crop mode.
+myImg.resize(fill().width(250).height(250));
+
+export default {
+    components: {
+        AdvancedImage,
+    },
+    data() {
+        return {
+        myImg,
+        };
+    },
+};
+
+</script>
+
+<template>
+  <div>
+    <AdvancedImage :cldImg="myImg" />
+  </div>
+</template>
